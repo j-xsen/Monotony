30ec20c64111a60406ceb3960326a7379bfac407
jaxsen <jaxsenhoneycutt@gmail.com>
2024-10-22 18:33:22 -0400

    Create test_stat.py

---
+++ tests/test_stat.py
@@ -0,0 +1,8 @@
+from objects.player.stat import Stat
+
+def test_change_value():
+    add_stat = Stat(100)
+    sub_stat = Stat(0)
+    add_stat.change_value(25)
+    sub_stat.change_value(-25)
+    assert add_stat.value == 100 and sub_stat.value == 0
